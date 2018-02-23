c4d363bb3f48f8d1bfd6bcf3ae3be2cd0345dbf2
iharmanpannu <iharmanpannu@gmail.com>
2018-02-23 12:50:15 -0800

    changes

--- js/script-transpiled.js
+++ js/script-transpiled.js
@@ -1,9 +1,20 @@
 "use strict";
 
+function _toConsumableArray(arr) {
+  if (Array.isArray(arr)) {
+    for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) {
+      arr2[i] = arr[i];
+    }
+    return arr2;
+  } else {
+    return Array.from(arr);
+  }
+}
+
 (function() {
-  const url = "http://api.openweathermap.org/data/2.5/weather?q=";
-  const apiKey = "05e62f86576a85bc20cce0e46674216f"; // Replace "APIKEY" with your own API key; otherwise, your HTTP request will not work
-  const activities = {
+  var url = "http://api.openweathermap.org/data/2.5/weather?q=";
+  var apiKey = "05e62f86576a85bc20cce0e46674216f"; // Replace "APIKEY" with your own API key; otherwise, your HTTP request will not work
+  var activities = {
     teamIn: ["basketball", "hockey", "volleyball"],
     teamOutWarm: [
       "softball/baseball",
@@ -25,26 +36,26 @@
       "ice skating"
     ]
   };
-  let state = {};
-  let category = "all";
+  var state = {};
+  var category = "all";
 
   // get weather data when user clicks Forecast button, then add temp & conditions to view
   //	$('.forecast-button').click(function(e) {
-  document.querySelector(".forecast-button").addEventListener(
-    "click",
-    function(e) {
+  document
+    .querySelector(".forecast-button")
+    .addEventListener("click", function(e) {
       e.preventDefault();
       //		const location = $('#location').val();
-      const location = document.querySelector("#location").value;
+      var location = document.querySelector("#location").value;
       //		$('#location').val('');
       document.querySelector("#location").value = "";
       /*
-		$.get(url + location + '&appid=' + apiKey).done(function(response) {
-			updateUISuccess(response);
-		}).fail(function() {
-			updateUIFailure();
-		});
-*/
+  		$.get(url + location + '&appid=' + apiKey).done(function(response) {
+  			updateUISuccess(response);
+  		}).fail(function() {
+  			updateUIFailure();
+  		});
+  */
 
       fetch(url + location + "&appid=" + apiKey)
         .then(function(response) {
@@ -56,9 +67,7 @@
         .catch(function() {
           updateUIFailure();
         });
-    },
-    false
-  );
+    });
 
   // update list of sports when user selects a different category (solo/team/all)
   //$('.options div').on('click', updateActivityList);
@@ -68,10 +77,10 @@
 
   // handle ajax success
   function updateUISuccess(response) {
-    const degC = response.main.temp - 273.15;
-    const degCInt = Math.floor(degC);
-    const degF = degC * 1.8 + 32;
-    const degFInt = Math.floor(degF);
+    var degC = response.main.temp - 273.15;
+    var degCInt = Math.floor(degC);
+    var degF = degC * 1.8 + 32;
+    var degFInt = Math.floor(degF);
     state = {
       condition: response.weather[0].main,
       icon:
@@ -82,41 +91,42 @@
     };
 
     //		const $into = $('.conditions')[0];
-    const into = document.querySelector(".conditions");
+    var into = document.querySelector(".conditions");
 
     //		ReactDOM.render(<Forecast {...state} />, $into);
     //		ReactDOM.render(<Forecast {...state} />, into);
 
     /*
-		function Forecast(props) {
-			return (
-				<div>
-					<p className="city">{props.city}</p>
-					<p>{props.degCInt}&#176; C / {props.degFInt}&#176; F <img src={props.icon} alt={props.condition} /></p>
-				</div>
-			)
-		}
-*/
+  		function Forecast(props) {
+  			return (
+  				<div>
+  					<p className="city">{props.city}</p>
+  					<p>{props.degCInt}&#176; C / {props.degFInt}&#176; F <img src={props.icon} alt={props.condition} /></p>
+  				</div>
+  			)
+  		}
+  */
 
-    let container = document.createElement("div");
-    let cityPara = document.createElement("p");
+    var fragment = document.createDocumentFragment();
+    var container = document.createElement("div");
+    var cityPara = document.createElement("p");
     cityPara.setAttribute("class", "city");
     cityPara.textContent = state.city;
-    let conditionsPara = document.createElement("p");
+    var conditionsPara = document.createElement("p");
     //		conditionsPara.textContent = state.degCInt + '\u00B0 C / ' + state.degFInt + '\u00B0 F';
-    conditionsPara.textContent = `${state.degCInt}\u00B0 C / ${
-      state.degFInt
-    }\u00B0 F`;
-    let iconImage = document.createElement("img");
+    conditionsPara.textContent =
+      state.degCInt + "\xB0 C / " + state.degFInt + "\xB0 F";
+    var iconImage = document.createElement("img");
     iconImage.setAttribute("src", state.icon);
     iconImage.setAttribute("alt", state.condition);
     conditionsPara.appendChild(iconImage);
     container.appendChild(cityPara);
     container.appendChild(conditionsPara);
+    fragment.appendChild(container);
     if (document.querySelector(".conditions div")) {
-      into.replaceChild(container, document.querySelector(".conditions div"));
+      into.replaceChild(fragment, document.querySelector(".conditions div"));
     } else {
-      into.appendChild(container);
+      into.appendChild(fragment);
     }
 
     updateActivityList();
@@ -163,50 +173,67 @@
 
     function updateState(type) {
       if (category === "solo") {
-        state.activities.push(...activities["solo" + type]);
+        var _state$activities;
+
+        (_state$activities = state.activities).push.apply(
+          _state$activities,
+          _toConsumableArray(activities["solo" + type])
+        );
       } else if (category === "team") {
-        state.activities.push(...activities["team" + type]);
+        var _state$activities2;
+
+        (_state$activities2 = state.activities).push.apply(
+          _state$activities2,
+          _toConsumableArray(activities["team" + type])
+        );
       } else {
-        state.activities.push(...activities["solo" + type]);
-        state.activities.push(...activities["team" + type]);
+        var _state$activities3, _state$activities4;
+
+        (_state$activities3 = state.activities).push.apply(
+          _state$activities3,
+          _toConsumableArray(activities["solo" + type])
+        );
+        (_state$activities4 = state.activities).push.apply(
+          _state$activities4,
+          _toConsumableArray(activities["team" + type])
+        );
       }
     }
 
     //		const $into = $('.activities')[0];
-    const into = document.querySelector(".activities");
+    var into = document.querySelector(".activities");
 
     //		ReactDOM.render(<Activities {...state} />, $into);
     //		ReactDOM.render(<Activities {...state} />, into);
 
     /*
-		function Activities(props) {
-			const activitiesList = props.activities.map(function(activity, index) {
-				return <li key={index}>{activity}</li>
-			});
-			return (
-				<div>
-					<ul>{activitiesList}</ul>
-				</div>
-			)
-		}
-*/
+  		function Activities(props) {
+  			const activitiesList = props.activities.map(function(activity, index) {
+  				return <li key={index}>{activity}</li>
+  			});
+  			return (
+  				<div>
+  					<ul>{activitiesList}</ul>
+  				</div>
+  			)
+  		}
+  */
 
-    let activitiesContainer = document.createElement("div");
-    let list = document.createElement("ul");
+    var fragment = document.createDocumentFragment();
+    var activitiesContainer = document.createElement("div");
+    var list = document.createElement("ul");
     state.activities.forEach(function(activity, index) {
-      let listItem = document.createElement("li");
+      var listItem = document.createElement("li");
       listItem.setAttribute("key", index);
       listItem.textContent = activity;
       list.appendChild(listItem);
     });
     activitiesContainer.appendChild(list);
+    fragment.appendChild(activitiesContainer);
     if (document.querySelector(".activities div")) {
-      into.replaceChild(
-        activitiesContainer,
-        document.querySelector(".activities div")
-      );
+      into.replaceChild(fragment, document.querySelector(".activities div"));
     } else {
-      into.appendChild(activitiesContainer);
+      into.appendChild(fragment);
     }
 
     //		$('.results').slideDown(300);
--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-(() => {
+"use strict";
+
+(function() {
   const url = "http://api.openweathermap.org/data/2.5/weather?q=";
   const apiKey = "05e62f86576a85bc20cce0e46674216f"; // Replace "APIKEY" with your own API key; otherwise, your HTTP request will not work
   const activities = {
@@ -27,27 +29,40 @@
   let category = "all";
 
   // get weather data when user clicks Forecast button, then add temp & conditions to view
-  const foreCastButton = document.querySelector(".forecast-button");
-
-  foreCastButton.addEventListener(
+  //	$('.forecast-button').click(function(e) {
+  document.querySelector(".forecast-button").addEventListener(
     "click",
-    e => {
+    function(e) {
       e.preventDefault();
+      //		const location = $('#location').val();
       const location = document.querySelector("#location").value;
+      //		$('#location').val('');
       document.querySelector("#location").value = "";
-      fetch(`${url + location}&appid=${apiKey}`)
-        .then(response => {
+      /*
+		$.get(url + location + '&appid=' + apiKey).done(function(response) {
+			updateUISuccess(response);
+		}).fail(function() {
+			updateUIFailure();
+		});
+*/
+
+      fetch(url + location + "&appid=" + apiKey)
+        .then(function(response) {
           return response.json();
         })
-        .then(response => updateUISuccess(response))
-        .catch(updateUIFailure());
+        .then(function(response) {
+          updateUISuccess(response);
+        })
+        .catch(function() {
+          updateUIFailure();
+        });
     },
     false
   );
 
   // update list of sports when user selects a different category (solo/team/all)
-  const options = document.querySelectorAll(".options div");
-  options.forEach(el => {
+  //$('.options div').on('click', updateActivityList);
+  document.querySelectorAll(".options div").forEach(function(el) {
     el.addEventListener("click", updateActivityList, false);
   });
 
@@ -59,26 +74,30 @@
     const degFInt = Math.floor(degF);
     state = {
       condition: response.weather[0].main,
-      icon: `http://openweathermap.org/img/w/${response.weather[0].icon}.png`,
+      icon:
+        "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png",
       degCInt: Math.floor(degCInt),
       degFInt: Math.floor(degFInt),
       city: response.name
     };
 
+    //		const $into = $('.conditions')[0];
     const into = document.querySelector(".conditions");
-    // ReactDOM.render(<Forecast {...state} />, into);
 
-    // function Forecast(props) {
-    //   return (
-    //     <div>
-    //       <p className="city">{props.city}</p>
-    //       <p>
-    //         {props.degCInt}&#176; C / {props.degFInt}&#176; F{" "}
-    //         <img src={props.icon} alt={props.condition} />
-    //       </p>
-    //     </div>
-    //   );
-    // }
+    //		ReactDOM.render(<Forecast {...state} />, $into);
+    //		ReactDOM.render(<Forecast {...state} />, into);
+
+    /*
+		function Forecast(props) {
+			return (
+				<div>
+					<p className="city">{props.city}</p>
+					<p>{props.degCInt}&#176; C / {props.degFInt}&#176; F <img src={props.icon} alt={props.condition} /></p>
+				</div>
+			)
+		}
+*/
+
     let container = document.createElement("div");
     let cityPara = document.createElement("p");
     cityPara.setAttribute("class", "city");
@@ -105,13 +124,14 @@
 
   // handle selection of a new category (team/solo/all)
   function updateActivityList(event) {
-    // if (event !== undefined && $(this).hasClass("selected")) {
-    if (event != undefined && event.target.classList.contains("selected")) {
+    //		if (event !== undefined && $(this).hasClass('selected')) {
+    if (event !== undefined && event.target.classList.contains("selected")) {
       // if the 'event' parameter is defined, then a tab has been clicked; if not, then this is the
       //   default case and the view simply needs to be updated
       // if the clicked tab has the class 'selected', then no need to change location of 'selected' class
       //   or change the DOM
       return true;
+      //		} else if (event !== undefined && !$(this).hasClass('selected')) {
     } else if (
       event !== undefined &&
       !event.target.classList.contains("selected")
@@ -119,12 +139,16 @@
       // if the 'event' parameter is defined, then a tab has been clicked
       // if the clicked tab does not have the class 'selected', then location of 'selected' class must be added
       //   to the clicked element and removed from its siblings
+      //			category = $(this).attr('id');
       category = event.target.id;
 
-      document.querySelectorAll(".options div").forEach(el => {
+      //			$('.options div').removeClass('selected');
+
+      document.querySelectorAll(".options div").forEach(function(el) {
         el.classList.remove("selected");
       });
 
+      //			$(this).addClass('selected');
       event.target.classList.add("selected");
     }
 
@@ -139,16 +163,33 @@
 
     function updateState(type) {
       if (category === "solo") {
-        state.activities.push(...activities[`solo${type}`]);
+        state.activities.push(...activities["solo" + type]);
       } else if (category === "team") {
-        state.activities.push(...activities[`team${type}`]);
+        state.activities.push(...activities["team" + type]);
       } else {
-        state.activities.push(...activities[`solo${type}`]);
-        state.activities.push(...activities[`team${type}`]);
+        state.activities.push(...activities["solo" + type]);
+        state.activities.push(...activities["team" + type]);
       }
     }
 
+    //		const $into = $('.activities')[0];
     const into = document.querySelector(".activities");
+
+    //		ReactDOM.render(<Activities {...state} />, $into);
+    //		ReactDOM.render(<Activities {...state} />, into);
+
+    /*
+		function Activities(props) {
+			const activitiesList = props.activities.map(function(activity, index) {
+				return <li key={index}>{activity}</li>
+			});
+			return (
+				<div>
+					<ul>{activitiesList}</ul>
+				</div>
+			)
+		}
+*/
 
     let activitiesContainer = document.createElement("div");
     let list = document.createElement("ul");
